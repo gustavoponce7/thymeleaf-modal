ab92ed3a0033cd398a438def9627f3d327c06a8f
Gustavo Ponce <gustavo_ponce@epam.com>
2018-02-05 17:16:42 +0300

    adding edit mode and style

--- src/main/java/com/example/thymeleafpoc/controller/StudentController.java
+++ src/main/java/com/example/thymeleafpoc/controller/StudentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -36,12 +37,21 @@ public class StudentController {
 		return modelAndView;
 	}
 	
+	@PostMapping(value="/edit-student")
+	public ModelAndView editStudent(@ModelAttribute Student student) {
+		studentService.saveStudent(student);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("students", studentService.getAllStudents());
+		modelAndView.setViewName("redirect:/students");
+		return modelAndView;
+	}
+	
 	/*
 	 * MODAL DIALOGS
 	 * 
 	 */
 	@GetMapping(value="/student/{id}")
-	public String getStudent(@PathVariable("id") long id, ModelMap model) {
+	public String getStudentModal(@PathVariable("id") long id, ModelMap model) {
 		Student student = studentService.getOneStudentById(id);
 		model.addAttribute("student", student);
 		return "modal/student :: student";
@@ -52,6 +62,13 @@ public class StudentController {
 		Student student = studentService.getOneStudentById(id);
 		model.addAttribute("student", student);
 		return "modal/delete-student :: delete-student";
+	}
+	
+	@GetMapping(value="/edit-student/{id}")
+	public String editStudentModal(@PathVariable("id") long id, ModelMap model) {
+		Student student = studentService.getOneStudentById(id);
+		model.addAttribute("student", student);
+		return "modal/edit-student :: edit-student";
 	}
 	/*
 	 * 
